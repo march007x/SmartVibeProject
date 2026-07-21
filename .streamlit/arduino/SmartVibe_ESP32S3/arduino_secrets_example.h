64fa7fc02e2c2ff84970617655cb7a0caa8e1fbf
march007x <supphawit080@gmail.com>
2026-07-21 20:33:31 +0700

    Create arduino_secrets_example.h

---
+++ .streamlit/arduino/SmartVibe_ESP32S3/arduino_secrets_example.h
@@ -0,0 +1,8 @@
+// ============================================================
+// ไฟล์ตัวอย่าง — ก๊อปปี้เป็น arduino_secrets.h แล้วเติมค่าจริง
+// ============================================================
+#pragma once
+
+#define WIFI_SSID      "abc"
+#define WIFI_PASSWORD  "123456789"
+#define FIREBASE_AUTH  "ppiUiqYSYNv0uJSuoDyie3rrPqT9DAA8yUXbFs2L"
