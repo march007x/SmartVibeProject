53b4771028b1c2cf47048f3e02d80fb768c0bda9
march007x <supphawit080@gmail.com>
2026-07-22 00:43:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ FIREBASE_DOMAIN = "ggggggggggggg-a4e95-default-rtdb.asia-southeast1.firebasedata
 DB_PATH = "History3F"          # ต้องตรงกับ DB_PATH ในโค้ด ESP32
 
 # 🔑 Database secret
-AUTH_TOKEN = "KJjhueluvwTjUFdovgXbItxMn6RI9i4jZ7Pv2uHA"
+AUTH_TOKEN = "tehEtKwtGntluiO9i6eOxuvZNNfjhrkf7GJ5pRBB"
 FIREBASE_URL = f"https://{FIREBASE_DOMAIN}/{DB_PATH}.json"
 QUERY = ('?orderBy="$key"&limitToLast=450' if not AUTH_TOKEN
          else f'?auth={AUTH_TOKEN}&orderBy="$key"&limitToLast=450')
